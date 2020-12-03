66619d9140079f7ee5ef2a8455a5bfe7be187450
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2020-12-03 16:17:16 +0300

    update products added reviews

--- products/.left.menu_ext.php
+++ products/.left.menu_ext.php
@@ -1,21 +1,19 @@
+
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
 global $APPLICATION;
-
 $aMenuLinksExt=$APPLICATION->IncludeComponent("bitrix:menu.sections", "", array(
-	"IS_SEF" => "Y",
-	"SEF_BASE_URL" => "",
-	"SECTION_PAGE_URL" => "#SECTION_ID#/",
-	"DETAIL_PAGE_URL" => "#SECTION_ID#/#ELEMENT_ID#",
-	"IBLOCK_TYPE" => "products",
-	"IBLOCK_ID" => "#PRODUCTS_IBLOCK_ID#",
-	"DEPTH_LEVEL" => "1",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000"
-	),
-	false
+    "IS_SEF" => "Y",
+    "SEF_BASE_URL" => "",
+    "SECTION_PAGE_URL" => "#SITE_DIR#/products/#SECTION_ID#/",
+    "DETAIL_PAGE_URL" => "#SITE_DIR#/products/#SECTION_ID#/",
+    "IBLOCK_TYPE" => "products_s1",
+    "IBLOCK_ID" => "3",
+    "DEPTH_LEVEL" => "3",
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "36000000"
+),
+    false
 );
-
-$aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
+$aMenuLinks = array_merge($aMenuLinksExt, $aMenuLinks);
 ?>
