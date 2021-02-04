b7ff9337621ee8c40ce35f08c7c74a74eeac22bb
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2021-02-04 14:27:40 +0300

    added

--- about_delivery.php
+++ about_delivery.php
@@ -3,38 +3,30 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("О доставке");
 ?>
 
-
 <script src="//glavpunkt.ru/js/punkts-widget/glavpunkt.js" charset="utf-8"></script>
 
 <a href="#" onclick="glavpunkt.openMap(selectPointDel, optionsPoint); return false;">
-  Выбрать пункт выдачи на карте
+    Выбрать пункт выдачи на карте
 </a>
 
 
-
 <script>
+    function selectPointDel(pointInfo) {
 
-  function selectPointDel(pointInfo) {
-   
-    const idPoint = pointInfo.id;
-    const adrPoint = pointInfo.address;
-    
-    alert( "ID:  " + idPoint + "\n" + "Address:  " + adrPoint);
+        const idPoint = pointInfo.id;
+        const adrPoint = pointInfo.address;
 
-  }
+        alert("ID: " + idPoint + "\n" + "Address: " + adrPoint);
 
-  const optionsPoint = {
+    }
+
+    const optionsPoint = {
         defaultCity: 'SPB',
-        excludeOperators : ['cdek'],
-        onlyCities: ['Санкт-Петербург', 'Новосибирск' ],
-}
+        excludeOperators: ['cdek'],
+        onlyCities: ['Санкт-Петербург', 'Новосибирск'],
 
+    }
 </script>
-
-
-
-
-
 
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
